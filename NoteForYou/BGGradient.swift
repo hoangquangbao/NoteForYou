405ebc62a907eda5eb6af4fc24b9de4bf6467970
Bao Hoang <hoangquangbao.developer@gmail.com>
2024-05-12 11:01:26 +0700

    init shape

---
+++ NoteForYou/BGGradient.swift
@@ -0,0 +1,18 @@
+//
+//  BGGradient.swift
+//  NoteForYou
+//
+//  Created by Bao Hoang on 12/5/24.
+//
+
+import SwiftUI
+
+struct BGGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BGGradient()
+}
